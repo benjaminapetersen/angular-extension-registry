eada1eccd89adaaea357e7db299016c1ebccc4ed
benjaminapetersen <admin@benjaminapetersen.me>
2017-05-01 11:29:48 -0400

    Support weighted items for ordering, bump node version from 4 to 6 for tests
    - Add a sorting function to the extensionRegistry.get() function
      - used internally by directives
    - Add unit tests to ensure sorting acts as expected
      - sort by weight
      - valid weights are Numbers, or Strings that can be cast to Numbers
      - undefined is treated as -1
        - both items without a weight & with an explicit `undefined`
      - NaN, { }, [ ], null & other invalid weights will be treated as weight: -9999
    - Add manual test to ensure delayed registration properly sorts
    	- TODO: write a unit test to cover this behavior
    - update README.md
    
    - Bump node version from 4 to 6 for travis/tests
    - use 'var' for tests instead of 'let' as travis has Firefox 31 installed:
    	- SyntaxError: let is a reserved identifier

--- demo/list-items/myApp.js
+++ demo/list-items/myApp.js
@@ -6,8 +6,43 @@
   ])
 
   .config([
-    function() {
-      // nothing to do here, ATM
+    'extensionRegistryUtils',
+    'extensionRegistryProvider',
+    function(utils, extensionRegistryProvider) {
+      // example custom comparator, which was the current comparator prototype
+      // similar to the built in, but more granular control sets invalid values to -9999.
+      var isString = utils.isString,
+          isNaN = utils.isNaN,
+          isNumber = utils.isNumber,
+          isUndefined = utils.isUndefined,
+          invalidVal = -9999,
+          strToUsableNum = function(val) {
+            var test = Number(val);
+            return isNaN(test) ?
+                    invalidVal :
+                    test;
+          },
+          toWeight = function(value) {
+            return isNaN(value) ?
+                    invalidVal :
+                    isNumber(value) ?
+                      value :
+                      isString(value) ?
+                        strToUsableNum(value) :
+                        isUndefined(value) ?
+                          -1 :
+                          invalidVal;
+          };
+      // can optionally override the comparator with a custom
+      // comparator function
+      extensionRegistryProvider.setComparator(function(a, b) {
+        console.log('Using custom comparator');
+        return toWeight(a.weight) - toWeight(b.weight);
+      });
+      // show that `add()` works up-front
+      extensionRegistryProvider.add('list', function() {
+        return [{type:"li", text: 'Lalala',    weight: 1  }];
+      });
     }
   ])
 
@@ -24,6 +59,6 @@
       //  <extension-point extension-type="text <new-type>"
       extensionRegistry.addType('li', '<li>{{item.text}}</li>');
     }
-  ])
+  ]);
 
 })();
